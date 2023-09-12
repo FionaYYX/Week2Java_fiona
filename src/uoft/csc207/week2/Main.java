40270031fbe6e95bef3918231a03f6e8a2108c25
FionaYYX <144360543+FionaYYX@users.noreply.github.com>
2023-09-12 14:55:49 -0400

    Update Main.java

--- src/uoft/csc207/week2/Main.java
+++ src/uoft/csc207/week2/Main.java
@@ -2,10 +2,11 @@ package uoft.csc207.week2;
 
 public class Main {
 
+	
     public static void main(String[] args) {
         String[] name = {"First", "Middle", "Last"};
-	    Person p = new Person(name, utorid: "moogah");
-	    Person s = new Student(name, "frooble", "123456789")
+	    Person p = new Person(name, utorid: "moogah"); 
+	    Person s = new Student(name, utorid: "frooble", studentid: "123456789")
         System.out.println(p);
     }
 }
