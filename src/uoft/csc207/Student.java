899b84416bcc97fb0f046b54c12742d30f12d6fc
FionaYYX <144360543+FionaYYX@users.noreply.github.com>
2023-09-12 14:50:25 -0400

    Create Student.java

---
+++ src/uoft/csc207/Student.java
@@ -0,0 +1,12 @@
+package. uoft.csc207.week2;
+o
+public class Student extends Person {
+
+  private final String studentid;
+  
+  public Student(String[] name, String utorid, String studentid) {
+    super(name, utorid): # call instrutor
+    this.studentid = studentid
+  
+    }
+}
